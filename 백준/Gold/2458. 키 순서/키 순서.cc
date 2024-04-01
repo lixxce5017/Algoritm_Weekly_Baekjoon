a733fb6589b739b16d3140ca096f73568c9d394e
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-04-01 20:40:51 +0900

    [Gold IV] Title: 키 순서, Time: 52 ms, Memory: 3364 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+bool visitied[501];
+vector<int> v[501]; // 자신보다 작은 학생
+vector<int> vd[501]; // 자신보다 큰 학생
+int order = 0;
+void dfs(vector<int>arr[], int node)
+{
+    visitied[node] = true;
+    for (auto i : arr[node])
+    {
+        if (visitied[i] == false)
+        {
+            visitied[i] = true;
+            order++;
+            dfs(arr,i);
+        }
+    }
+    return;
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        vd[b].push_back(a);
+    }
+    int res = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        order = 0;
+        memset(visitied, false, sizeof(visitied));
+        dfs(v,i);
+        memset(visitied, false, sizeof(visitied));
+        dfs(vd,i);
+        if (order == n - 1)
+        {
+            res += 1;
+        }
+    }
+    cout << res;
+    return 0;
+}
