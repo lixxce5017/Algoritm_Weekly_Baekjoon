837852bb9aad40d59a0c31651ce7e8283e6e8aa6
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-04-29 04:18:42 +0900

    [Platinum I] Title: 연속합과 쿼리, Time: 88 ms, Memory: 78192 KB -BaekjoonHub

---
+++ 백준/Platinum/16993. 연속합과 쿼리/연속합과 쿼리.cc
@@ -0,0 +1,98 @@
+#include <iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+#define MAX 500004
+typedef long long ll;
+struct fuck_seg
+{
+	ll one; ll two; ll three; ll total2;
+};
+
+int arr[MAX];
+fuck_seg tree[MAX * 4];
+int lazy[MAX * 4];
+int in[MAX];
+int out[MAX];
+int cnt = 1;
+//걍 만들어 놓고 쿼리만 받음 될 듯
+//최솟값 최댓값 찾는 문제처럼 max 
+//구간을 어떻게 끌고오지?
+
+//아 다 합친 경의 것도 저장 왼쪽만 선택한 것도 저장 다 비교 때리기
+void init(int node, int start, int end)
+{
+	if (start == end)
+	{
+		tree[node].one = arr[start];
+		tree[node].two = arr[start];
+		tree[node].three = arr[start];
+		tree[node].total2 = arr[start];
+		return;
+	}
+	int mid = (start + end) / 2;
+
+	init(node * 2, start, mid);
+	init(node * 2 + 1, mid + 1, end);
+	//두개 더한거과 왼쪽만 더큰 것?
+	//두개 더한거과 오른쪽 더 큰 것?
+	//이따구로 가면 안된다
+	//토탈 토탈 맥스도 해야되나? 일단해보고 안되면 ㄱ
+	tree[node].two = max( tree[node * 2].two, max(tree[node * 2 + 1].two, tree[node * 2].three + tree[node * 2 + 1].one ));
+	tree[node].one = max(tree[node * 2].one, tree[node * 2+1].one+tree[node*2].total2);
+	tree[node].three = max(tree[node * 2+1].three, tree[node * 2].three+tree[node*2+1].total2);
+	tree[node].total2 = tree[node * 2 + 1].total2 + tree[node * 2].total2;
+	return;
+}
+
+
+fuck_seg getQuery(int node, int start, int end, int lidx, int ridx)
+{
+	
+	if (start > ridx || end < lidx)
+	{
+		 fuck_seg fake = { -INT_MAX,-INT_MAX, -INT_MAX ,-INT_MAX};
+		 return fake;
+	}
+	if (start >= lidx && end <= ridx)
+	{
+		return tree[node];
+	}
+	int mid = (start + end) / 2;
+	fuck_seg ans1 = getQuery(node * 2, start, mid, lidx, ridx);
+	fuck_seg ans2 = getQuery(node * 2 + 1, mid + 1, end, lidx, ridx);
+	fuck_seg ans3;
+	ans3.two = max(ans1.three + ans2.one, max(ans1.two, ans2.two));
+	ans3.one = max(ans1.one, ans2.one + ans1.total2);
+	ans3.three = max(ans2.three, ans1.three + ans2.total2);
+	ans3.total2 = ans2.total2 + ans1.total2;
+	//토탈끼리 다 더한게 크냐
+	return ans3;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+	int n, m;
+	cin >> n;
+	int start = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cin >> m;
+	init(1, 1, n);
+
+	for (int i = 0; i < m; i++)
+	{
+	int b, c;
+	cin >> b >> c;
+	fuck_seg ww= getQuery(1, 1, n, b, c);
+	cout << max(ww.total2,max(ww.one,max(ww.two, ww.three))) << "\n";
+	}
+
+
+	return 0;
+}
