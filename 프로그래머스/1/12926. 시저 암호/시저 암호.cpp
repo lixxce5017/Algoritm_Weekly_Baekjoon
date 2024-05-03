963cd84831dbc7f9e4e06d62e36950abbb7bdd9d
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-05-03 16:39:37 +0900

    [level 1] Title: 시저 암호, Time: 0.11 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]>='a'&&s[i]<='z')
+        {
+            if(s[i]+n>'z')
+             answer+=s[i]+n-26;
+            else
+            {
+                answer+=s[i]+n;
+            }
+        }
+        else if(s[i]>='A'&& s[i]<='Z')
+        {
+             if(s[i]+n>'Z')
+             answer+=s[i]+n-26;
+            else
+            {
+                answer+=s[i]+n;
+            }
+        }
+        else if(s[i]==' ')
+        {
+            answer+=' ';
+        }
+
+    }
+    return answer;
+}
