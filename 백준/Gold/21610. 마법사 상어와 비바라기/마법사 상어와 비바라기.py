5d5ef3277048b798373752d8456e385f2ca5609d
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-05-20 16:13:12 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 500 ms, Memory: 112536 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,46 +1,48 @@
+import sys
 n,m= map(int,input().split())
-arr=[list(map(int,input().split())) for _ in range(n)]
-
-dis=[]
-for i in range(m):
-    a,b= map(int,input().split())
-    dis.append((a,b))
-
-coluds=[(n-1,0),(n-1,1),(n-2,0),(n-2,1)]
+Map=[]
+for _ in range(n):
+     tmp=list(map(int,(input().split())))
+     Map.append(tmp)
 dx8=("empty",0,-1,-1,-1,0,1,1,1)
 dy8=("empty",-1,-1,0,1,1,1,0,-1)
+qurey=[]
 dist=[(-1,-1),(-1,1),(1,1),(1,-1)]
-cloud=[]
-for d,s in dis:
-    cloud.clear()
+for _ in range(m):
+    a,b=map(int,input().split())
+    qurey.append((a,b))
+# d번 방향으로 s칸 이동
+coluds=[(n-1,0),(n-1,1),(n-2,0),(n-2,1)]
+for d,s in qurey:
+    cloud = []
     for x,y in coluds:
         nx=(x+dx8[d]*s)%n
         ny=(y+dy8[d]*s)%n
-        arr[nx][ny]+=1
+        nx=nx
+        ny=ny
+        Map[nx][ny]+=1
         cloud.append((nx,ny))
 
     for cx,cy in cloud:
         for dx,dy in dist:
-            rx=cx+dx
-            ry=cy+dy
-            if n > rx >= 0 and n > ry >= 0:
-                if arr[rx][ry]>0:
-                    arr[cx][cy]+=1
-
+            nx=dx+cx
+            ny=dy+cy
+            if nx>=0 and ny >=0 and nx<n and ny<n:
+                if Map[nx][ny]!=0:
+                    Map[cx][cy]+=1
+    cnt=0
     newMap=[]
     for i in range(n):
         for j in range(n):
-            if arr[i][j]<2 or (i,j) in cloud:
-                continue
-            else:
-                newMap.append((i, j))
-                arr[i][j] = arr[i][j] - 2
+                if Map[i][j]<2 or (i,j) in cloud :
+                    continue
+                newMap.append((i,j))
+                Map[i][j]=Map[i][j]-2
 
     coluds=newMap
 
-cnt = 0
 for i in range(n):
     for j in range(n):
-        cnt+=arr[i][j]
+        cnt+=Map[i][j]
 
 print(cnt)
