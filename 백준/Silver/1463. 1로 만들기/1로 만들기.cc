fef1453d5178b4cf6967e857cf5441e5ae703be2
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-06-05 22:56:49 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,29 +1,26 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
 using namespace std;
+int dp[1000003];
 
-int DP[1000000];
-
-int main() {
-	int N;
-	cin >> N;
-
-	for (int i = 2; i <= N; i++) {
-		// 1. 1을 뺀다.
-		DP[i] = DP[i - 1] + 1;
-
-		// 2. 2로 나누어지면
-		if (i % 2 == 0) {
-			DP[i] = min(DP[i], DP[i / 2] + 1);
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0)
+		{
+			dp[i] = min(dp[i / 3] + 1,dp[i]);
+		}
+		 if (i % 2 == 0)
+		{
+			dp[i] = min(dp[i / 2] + 1, dp[i]);
 		}
 
-		// 3. 3으로 나누어지면
-		if (i % 3 == 0) {
-			DP[i] = min(DP[i], DP[i / 3] + 1);
-		}
 	}
-
-	cout << DP[N];
-
-	return 0;
+	cout << dp[n];
 }
