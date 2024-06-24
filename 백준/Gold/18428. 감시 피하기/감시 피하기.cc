8fd9a6ae3a6c7e69eb9eb1597c541f2366166d59
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-06-24 16:14:27 +0900

    [Gold V] Title: 감시 피하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.cc
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+using namespace std;
+char arr[7][7];
+bool check[100];
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { 1,-1,0,0 };
+vector<pair<int, int>> Student;
+int n;
+bool YoN;
+void simutaion()
+{
+	
+	for (int k = 0; k < Student.size(); k++)
+	{
+		int x = Student[k].first;
+		int y = Student[k].second;
+		for (int i = 0; i < 4; i++)
+		{
+			 int nx = x + dx[i];
+			 int ny = y + dy[i];
+			while (nx >= 0 && ny >=0 && nx < n && ny < n)
+			{
+				
+				if (arr[nx][ny] == 'S')
+				{
+					//cout << "d";
+					return;
+				}
+				else if (arr[nx][ny] == 'W')
+				{
+					break;
+
+				}
+				nx += dx[i];
+				ny += dy[i];
+				}
+			}
+		}
+	/*for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}*/
+	//cout << "\n";
+	cout << "YES";
+	exit(0);
+	return;
+	}
+
+
+void dfs(int cnt,int idx)
+{
+	if (cnt == 3)
+	{
+		simutaion();
+		return;
+	}
+	for (int i = 0; i < n*n; i++)
+	{
+		if (check[i]==false&&arr[i/n][i%n]=='X')
+		{
+			check[i] = true;
+			arr[i/n][i%n] = 'W';
+			dfs(cnt + 1,i+1);
+			arr[i/n][i%n] = 'X';
+			check[i] = false;
+		}
+	}
+}
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+			if (arr[i][j] == 'T')
+			{
+				Student.push_back({ i,j });
+			}
+		}
+	}
+	dfs(0,0);
+	cout << "NO";
+}
