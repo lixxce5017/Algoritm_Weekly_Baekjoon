be9f36acf42ddc1e67f8f551ce55ba2073bd28d2
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-06-25 14:49:49 +0900

    [Gold V] Title: 로봇 청소기, Time: 192 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,33 +1,37 @@
-from collections import deque
+def recr(x,y,d):
+    global ans
+    #print(x,y)
+    if arr[x][y]==0:
+        ans+=1
+        arr[x][y]=2
+    nd=d
+    for i in range(4):
+        nd=(nd+3)%4
+        nx=x+dx[nd]
+        ny=y+dy[nd]
+        if  nx>=0 and ny>=0 and nx<n and ny<m and arr[nx][ny]==0:
+                recr(nx,ny,nd)
+                return
+    
+    x=x-dx[d]
+    y=y-dy[d]
+    if arr[x][y]!=1:
+        recr(x,y,d)
 
-n,m=map(int,input().split())
-sx,sy,d=map(int,input().split())
-Map=[list(map(int,input().split()))for _ in range(n)]
-visitied=[[0 for _ in range(m)] for _ in range(n)]
-diretion=[(-1,0),(0,1),(1,0),(0,-1)]
+        return
 
-cnt=1
-queue=deque()
-queue.append((sx,sy))
-visitied[sx][sy]=1
-while queue:
-    x,y= queue.popleft()
-    goga=0
-    for dx,dy in diretion:
-        d=(d+3)%4
-        nx=x+diretion[d][0]
-        ny=y+diretion[d][1]
-        if 0<=nx<n and 0<=ny<m:
-            if visitied[nx][ny]==0 and Map[nx][ny]==0:
-                queue.append((nx,ny))
-                visitied[nx][ny]=1
-                cnt+=1
-                goga=1
-                break
-    if goga == 0:
-        if Map[x - diretion[d][0]][y - diretion[d][1]] == 0:
-            queue.append((x - diretion[d][0], y - diretion[d][1]))
-        else:
-            print(cnt)
-            break
 
+ans=0
+n,m= map(int,input().split())
+sx,sy,d= map(int,input().split())
+arr = []
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+#북동남서
+dx= [-1,0,1,0]
+dy= [0,1,0,-1]
+
+
+recr(sx,sy,d)
+
+print(ans)
