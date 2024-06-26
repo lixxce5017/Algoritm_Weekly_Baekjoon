751f5ecc3bff1e2e0884750ec79806951dfd5a40
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-06-26 16:21:09 +0900

    [Platinum III] Title: 주유소, Time: 324 ms, Memory: 32520 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/28219. 주유소/주유소.cc
+++ 백준/Platinum/28219. 주유소/주유소.cc
@@ -3,59 +3,47 @@
 #include<vector>
 using namespace std;
 vector<vector<int>> arr;
-vector<vector<int>> tree_arr;
 bool visitied[200004];
+int dp[200004];
 int n, k;
 int ret = 0;
-void tree(int node)
-{
-    visitied[node] = true;
-    for (auto i : arr[node])
+
+void dfs(int cur) {
+    int cnt = 0;
+    visitied[cur] = true;
+    vector<int> res;
+    for (auto i : arr[cur])
     {
         if (visitied[i] == false)
         {
-            tree_arr[node].push_back(i);
-            tree(i);
+            dfs(i);
+            dp[cur] = max(dp[cur], dp[i] + 1);
+            res.push_back(dp[i]+1);
         }
     }
-}
-// 단방향 그래프인 트리라  필요없다 visitied
-int dfs(int cur) {
-    int cnt = 0;
-    vector<int> res;
-    //cout << cur << " ";
-    for (auto i : tree_arr[cur])
-    {
-        int temp = dfs(i);
-        res.push_back(temp);
-    }
-    sort(res.begin(), res.end(), greater<int>());
-    if (res.size() > 1)
-        cnt = max(cnt, res[0] + 1 + res[1] + 1);
-    else if (res.size() == 1)
-        cnt = max(cnt, res[0] + 1);
+    sort(res.rbegin(), res.rend());
+    if (res.size() == 1)
+        cnt = max(cnt, res[0]);
+    else if (res.size() > 1)
+        cnt = max(cnt, res[0] + res[1]);
     if (cnt >= k)
     {
         ret++;
-        cnt = -1;
+        dp[cur]=-1;
     }
-    return cnt;
 }
 
 int main() {
 
     cin >> n >> k;
-    arr.resize(n + 1);
-    tree_arr.resize(n + 1);
-    for (int i = 0; i < n-1; i++)
+    arr.resize(200004);
+    for (int i = 0; i < n - 1; i++)
     {
         int a, b;
         cin >> a >> b;
         arr[a].push_back(b);
         arr[b].push_back(a);
     }
-    visitied[1] = true;
-    tree(1);
     dfs(1);
     cout << ret;
 }
