b8f6e977c0537ca3db8f686999424aaf64dbd47b
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-06-27 13:18:29 +0900

    [Gold II] Title: 뉴스 전하기, Time: 0 ms, Memory: 2512 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
@@ -1,51 +1,61 @@
-#include<iostream>
-#include<vector>
 #include <algorithm>
+#include <vector>
+#include<cstring>
+#include <iostream>
+typedef long long ll;
 using namespace std;
-vector<vector<int>>arr;
+vector<vector<int>> arr;
+//dfs는 기본적으로 아무대나 가도된다.
+//그렇다면 그 아무대나 가는것에 대해서 기준을 정렬로 잡아주고
+//가는곳을 다 list에 넣고 문제에서 원하는 순서와 비교하는 것이다.
+//order[a]=i란 a번째 정점은 i번째로 가야한다 
+//path[i]=a dfs에서 i번째 방문은 a번째 노드이다.
 
-vector<int> val_ans;
-bool visitied[1000003];
-int dfs(int n,int chichan)
+bool chk[100003];
+int order[100003];
+int cnt = 0;
+
+
+int dfs(int node)
 {
-	if (arr[n].empty())
-	{
-		return 1;
-	}vector<int> val;
-	for (int i = 0; i < arr[n].size(); i++)
-	{
-		val.push_back(dfs(arr[n][i], 1) + 1);
-	}
-	int ret = 0;
-	sort(val.begin(), val.end(), greater<int>());
-	for (int i = 0; i < val.size(); i++)
-	{
-		ret = max(ret, val[i] + i);
-	}
-	return ret;
+    //cout << node << " ";
+    int cnt =0;
+    vector<int> list;
+    for (auto i : arr[node])
+    {
+        list.push_back(dfs(i) + 1);
+    }
+    sort(list.begin(), list.end(), greater<int>());
+    for (int i = 0; i < list.size(); i++)
+    {
+        list[i] = list[i] + i;
+        cnt = max(list[i], cnt);
+    }
+    return cnt;
 }
 int main()
 {
-	int n, m;
-	cin >> n;
-	arr.resize(n + 1);
-	val_ans.resize(n + 1, 0);
-	for (int i = 0; i < n; i++)
-	{
-		int temp;
-		cin >> temp;
-		if (temp == -1)
-			continue;
-		arr[temp].push_back(i);
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    arr.resize(n + 1);
+    int start = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        if (a == -1)
+        {
+            start = i;
+        }
+        else
+        {
+            arr[a].push_back(i);
+        }
+    }
 
-	/*for (int i = 1; i <= n; i++)
-	{
-		cout << val[i] << " ";
-	}cout << "\n";*/
-
-		cout<<dfs(0,1)-1;
-		//memset(visitied, false, sizeof(visitied));
-
+    cout << dfs(start);
 
 }
