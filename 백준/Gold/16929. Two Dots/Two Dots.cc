9f2a03f1670f63ab5da8a978f523f11f9ec0a5f4
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-07-05 17:33:15 +0900

    [Gold IV] Title: Two Dots, Time: 8 ms, Memory: 2512 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.cc
+++ 백준/Gold/16929. Two Dots/Two Dots.cc
@@ -1,84 +1,72 @@
-#include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <iostream>
 #include <cstring>
-#include<stack>
-#include<climits>
+#include <string>
 
-#define MAX 1000001
-typedef long long ll;
 using namespace std;
-char val[1003][1003];
-int visitied[1003][1003];
-int chk[1003][1003];
-int dx[4] = { 1,-1,0,0 };
-int dy[4] = { 0,0,-1,1 };
-int n, m;
-int cnt;
-bool truth = false;
-void dfs(int x, int y, char value, int count, int count2, int perx,int pery)
-{
-    //1이 다운
-    chk[x][y] = count2;
 
+bool visited[501][501] = { false, };
+
+char arr[501][501] = { 0, };
+
+int maxx = 0, n, m, ans = 0, flag = -1;
+
+int start_x, start_y;
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+
+void dfs(int x, int y, int depth)
+{
+  
     //cout << x << " " << y << "\n";
     for (int i = 0; i < 4; i++)
     {
-        int nx = dx[i] + x;
-        int ny = dy[i] + y;
-        visitied[nx][ny] = cnt;
-        if (nx < n && ny < m && nx >= 0 && ny >= 0)
+        int xx = x + dx[i];
+        int yy = y + dy[i];
+        if (xx == start_x && yy == start_y&& visited[xx][yy]==true&& depth >= 4)
         {
-            //가지치기 1
-            if (val[nx][ny] != value)
-            {
-                continue;
-            }
-            else if (nx == perx && ny == pery && chk[nx][ny] == count2 && count >= 4 && val[nx][ny] == value)
-            {
-                cout << "Yes";
-                exit(0);
-                return;
-            }
-            //뒤로 빠꾸해 오는걸 어찌 막지?
-            else if (val[nx][ny] == value && chk[nx][ny] == 0)
-            {
-                dfs(nx, ny, value, count + 1, count2, perx, pery);
+            //cout << "d";
+            flag = 1;
+            cout<< "Yes";
+            exit(0);
+        }
 
-            }
-                
+        if (visited[xx][yy] == false && xx >= 0 && yy >= 0 && xx < n && yy < m && arr[x][y] == arr[xx][yy])
+        {
+            visited[xx][yy] = true;
+            dfs(xx, yy, depth + 1);
+            visited[xx][yy] = false;
         }
     }
-    cnt++;
-    
 }
 
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int main()
+{
+    char a;
 
-    cin >> n>>m;
-    for (int i = 0; i < n; i++)
-    {
-        string s;
-        cin >> s;
-        for (int j = 0; j < s.size(); j++)
-        {
-            val[i][j] = s[j];
-        }
-    }
-    //집합?
-    int q=0;
+    cin >> n >> m;
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-  
-                q++;
-                dfs(i, j, val[i][j], 1, q,i,j);
-                memset(chk, 0, sizeof(chk));
+            cin >> a;
+            arr[i][j] = a;
         }
     }
-    cout << "No";
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            visited[i][j] = true;
+            start_x = i;
+            start_y = j;
+            dfs(i, j, 1);
+            visited[i][j] = false;
+        }
+    }
+        cout << "No";
 }
