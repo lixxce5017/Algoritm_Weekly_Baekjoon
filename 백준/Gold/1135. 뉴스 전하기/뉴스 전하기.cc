9b3db62a5a4c12eec8f4a233dcb38fce0ec4c4a1
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-07-20 01:58:21 +0900

    [Gold II] Title: 뉴스 전하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
@@ -1,61 +1,63 @@
-#include <algorithm>
-#include <vector>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<tuple>
+#include<limits.h>
+#include<queue>
 #include<cstring>
-#include <iostream>
-typedef long long ll;
+#include<vector>
 using namespace std;
+//vector<vector<int>> arr;
 vector<vector<int>> arr;
-//dfs는 기본적으로 아무대나 가도된다.
-//그렇다면 그 아무대나 가는것에 대해서 기준을 정렬로 잡아주고
-//가는곳을 다 list에 넣고 문제에서 원하는 순서와 비교하는 것이다.
-//order[a]=i란 a번째 정점은 i번째로 가야한다 
-//path[i]=a dfs에서 i번째 방문은 a번째 노드이다.
-
-bool chk[100003];
-int order[100003];
-int cnt = 0;
-
-
+bool visitied[53];
 int dfs(int node)
 {
-    //cout << node << " ";
-    int cnt =0;
-    vector<int> list;
-    for (auto i : arr[node])
-    {
-        list.push_back(dfs(i) + 1);
-    }
-    sort(list.begin(), list.end(), greater<int>());
-    for (int i = 0; i < list.size(); i++)
-    {
-        list[i] = list[i] + i;
-        cnt = max(list[i], cnt);
-    }
-    return cnt;
+	vector<int> val;
+	for (auto i : arr[node])
+	{
+		if (visitied[i] == false)
+		{
+			visitied[i] = true;
+			val.push_back(dfs(i));
+		}
+	}
+	sort(val.begin(), val.end(), greater<int>());
+	int time = 1;
+	int cnt = 0;
+	for (int i = 0; i < val.size(); i++)
+	{
+		val[i] += time;
+		time++;
+		cnt = max(val[i], cnt);
+	}
+	return cnt;
 }
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int n;
-    cin >> n;
-    arr.resize(n + 1);
-    int start = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int a;
-        cin >> a;
-        if (a == -1)
-        {
-            start = i;
-        }
-        else
-        {
-            arr[a].push_back(i);
-        }
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-    cout << dfs(start);
-
+	int n;
+	cin >> n;
+	arr.resize(n + 1);
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		if (num == -1)
+		{
+			continue;
+		}
+		else
+		{
+			arr[num].push_back(i);
+			arr[i].push_back(num);
+		}
+	}
+	visitied[0] = true;
+	cout<<dfs(0);
 }
