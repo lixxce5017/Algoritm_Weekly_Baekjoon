3218b204cad9194cdaec400406a780e985eb3d9c
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-08-02 13:58:53 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -4,60 +4,70 @@
 #include <stack>
 #include <string>
 #include <cmath>
-#include<cstring>
-#include<limits.h>
+#include <cstring>
+#include <queue>
 using namespace std;
-bool visitied[53][53];
-int arr[53][53];
-//북 ,동,남,서
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,1,0,-1 };
-int n, m,sx,sy,d;
-int cnt = 0;
-//좌표 조심하자 반시계이다 그리고 들어온 곳 그대로 빼주면서 나가야한다
-void dfs(int x, int y, int dir)
+int arr[51][51];
+bool visitied[51][51];
+int n, m;
+//북,남,서,동
+int dx[4] = { -1,0 , 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int ans = 0;
+void dfs(int x, int y, int d)
 {
-	if (visitied[x][y] == false)
-	{
-		cnt++;
-		visitied[x][y] = true;
-	}
-	bool falg = false;
-	for (int i = 0; i < 4; i++)
-	{
-		int dd = (dir-i+3) % 4;
-		int nx = dx[dd] + x;
-		int ny = dy[dd] + y;
-		if (visitied[nx][ny] == false && arr[nx][ny] == 0)
-		{
-			falg = true;
-			dfs(nx, ny, dd);
-		}
-	}
-	if (falg == false)
-	{
-		if (arr[x -dx[dir]][y - dy[dir]] == 0)
-		{
-			dfs(x -dx[dir], y -dy[dir], dir);
-		}
-		else
-		{
-			cout << cnt;
-			exit(0);
-		}
-
-	}
-	return;
+    if (visitied[x][y] == false)
+    {
+        visitied[x][y] = true;
+        ans++;
+    }
+    bool falg = true;
+    for (int i = 0; i < 4; i++)
+    {
+        //반시계다 주의
+        //반시계는 -1에 %4를 하여 -1이 나올 수 있으니 주의가 필요하다.
+        int dir = (d - i + 3) % 4;
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        
+        if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+            continue;
+        if (visitied[nx][ny] == true || arr[nx][ny] == 1)
+            continue;
+        falg = false;
+        dfs(nx, ny, dir);
+    }
+    if (falg)
+    {
+        if (arr[x - dx[d]][y - dy[d]] == 0)
+        {
+            dfs(x - dx[d], y - dy[d], d);
+        }
+        else
+        {
+            cout << ans << "\n";
+            exit(0);
+        }
+       return;
+    }
 }
-int main()
-{
-	cin >> n >> m>>sx>>sy>>d;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> arr[i][j];
-		}
-	}
-	dfs(sx, sy, d);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+        cin >> n >> m;
+    int sx, sy,q;
+    cin >> sx >> sy>>q;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+  
+
+    dfs(sx, sy,q);
+    cout << ans;
 }
