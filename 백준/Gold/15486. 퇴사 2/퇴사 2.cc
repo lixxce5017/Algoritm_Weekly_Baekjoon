d9f3ab1ebb75409eda357e2c331803882597ee30
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-08-13 15:10:03 +0900

    [Gold V] Title: 퇴사 2, Time: 780 ms, Memory: 19600 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <string>
 #include<math.h>
+#include<cstring>
+#include<climits>
 #include<vector>
 using namespace std;
 
-long long timee[1500003] = { 0, };
-long long val[1500003] = { 0, };
-long long dp[1500003] = { 0, };
+int dp[1500003];
+int t[1500003];
+int val[1500003];
 int main() {
-	
-	int n;
-	cin >> n;
 
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> timee[i] >> val[i];
-	}
-	for (int i = n; i >= 1; i--)
-	{
-		int line = i + timee[i];
-		if (line >n+1)
-		{
-			dp[i] = dp[i + 1];
-		}
-		else
-		{
-			dp[i] = max(dp[i + 1], val[i] + dp[line]);
-	
-		}
 
-	}
-	cout << dp[1];
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> t[i] >> val[i];
+    }
+    int time = n;
+    int res = 0;
+    for(int i=time;i>=0;i--)
+    {
+        dp[i] = dp[i + 1];
+       if (i + t[i] <= time + 1)
+        {
+            dp[i] = max(dp[i +t[i]] + val[i], dp[i +1]);
+        }
+        res = max(res, dp[i]);
+    }
+    cout << res;
+   
+    return 0;
 }
