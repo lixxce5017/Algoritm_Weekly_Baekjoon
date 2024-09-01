245926c0d9edfd1ee67cfa5914c2c88aaab90298
lixxce5017 <101561741+lixxce5017@users.noreply.github.com>
2024-09-01 16:52:30 +0900

    [Gold V] Title: 보물섬, Time: 584 ms, Memory: 118688 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,38 +1,35 @@
-from collections import deque
-
-n,m= map(int,input().split())
-arr=[]
-for i in range(n):
-    arr.append(list(input()))
-dx= [0,1,0,-1]
-dy = [1,0,-1,0]
+import collections
+n,m=map(int,input().split())
+Map=[list(input().rstrip())for _ in range(n)]
 
 
-def bfs(i,j):
-    queue= deque()
-    queue.append((i,j))
-    visitied = [[0] * m for _ in range(n)]
-    visitied[i][j]=1
-    ans=0
-    while queue:
-        x,y=queue.popleft()
+def bfs(q,w):
+    visitied=[[0]*m for _ in range(n)]
+    que=collections.deque()
+    que.append((q,w))
+    visitied[q][w]=1
+    dx=[0,0,-1,1]
+    dy=[1,-1,0,0]
+    res=0
+    while que:
+        x,y=que.popleft()
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if nx>=0 and ny>=0 and nx<n and ny<m :
-                if visitied[nx][ny]==0 and arr[nx][ny]=="L":
-                    visitied[nx][ny]= visitied[x][y]+1
-                    ans= max(ans,visitied[x][y])
-                    queue.append((nx,ny))
-    return ans
+            nx= dx[i]+x
+            ny=dy[i]+y
+            if nx<0 or ny<0 or nx>=n or ny>=m:
+                continue
+            if visitied[nx][ny]!=0 or Map[nx][ny]=='W':
+                continue
+            que.append((nx,ny))
+            visitied[nx][ny]=visitied[x][y]+1
+            res=max(res,visitied[nx][ny])
+    return res
 
-res =0
-
-
+ans=0
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == "L":
-            res=max(res,bfs(i,j))
-    if res == int(i) +int(j) - 2:
-        break
-print(res)
+        if Map[i][j]=='L':
+            q=bfs(i,j)
+            ans=max(ans,q)
+
+print(ans-1)
