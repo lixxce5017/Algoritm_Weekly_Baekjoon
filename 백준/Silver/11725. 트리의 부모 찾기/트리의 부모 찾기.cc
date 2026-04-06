c376597e36da2d60ff7d764ea57036eb8ea9f10d
뚱환 <101561741+lixxce5017@users.noreply.github.com>
2026-04-07 00:03:42 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 52 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,57 +1,54 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>
-#include <vector>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<deque>
+#include<queue>
+#include<stack>
+#include<tuple>
+#include<limits.h>
 using namespace std;
-vector<vector<int>>Arr;
+vector<vector<int>> arr;
+vector<int> parent;
 vector<bool>visitied;
-vector<int>result;
-void bfs()
-{
-    queue<int> qqq;
-    qqq.push(1);
-    visitied[1];
-    while (!qqq.empty())
-    {
-        int now = qqq.front();
-        qqq.pop();
-        for (int i : Arr[now])
-        {
-            if (visitied[i]== false)
-            {   
-                result[i] = now;
-                visitied[i] = true;
-                qqq.push(i);
-            }
-        }
 
+void dfs(int n)
+{
+    visitied[n] = true;
+    for (auto i : arr[n])
+    {
+        if (visitied[i] == false)
+        {
+            parent[i] = n;
+             dfs(i);
+        }
     }
 
-
 }
-int main(void)
-{
-
+int main(void) {
+  
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n;
+    int n, m;
     cin >> n;
-    Arr.resize(n + 1);
-    visitied.resize(n + 1, false);
-    result.resize(n + 1);
+    arr.resize(n + 1);
 
-    for (int i = 0; i < n-1; i++)
+    for (int i = 1; i < n; i++)
     {
         int a, b;
         cin >> a >> b;
-        Arr[a].push_back(b);
-        Arr[b].push_back(a);
+        arr[a].push_back(b);
+        arr[b].push_back(a);
     }
-    bfs();
+    visitied.resize(n + 1);
+    parent.resize(n + 1);
+    dfs(1);
     for (int i = 2; i <= n; i++)
     {
-        cout << result[i] << "\n";
+        cout << parent[i] << "\n";
     }
-  
+   
+    return 0;
 }
+
